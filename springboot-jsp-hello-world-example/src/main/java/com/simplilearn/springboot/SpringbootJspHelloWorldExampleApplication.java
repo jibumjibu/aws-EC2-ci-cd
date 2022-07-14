4e22d46c65310dd08040a5cb39063fc1c353367e
thejmj79 <johnmjibu@gmail.com>
2022-07-14 00:14:12 -0300

    final version

---
+++ springboot-jsp-hello-world-example/src/main/java/com/simplilearn/springboot/SpringbootJspHelloWorldExampleApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJspHelloWorldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJspHelloWorldExampleApplication.class, args);
+	}
+
+}
